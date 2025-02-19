0743e10015eb99ef320238fa69e4ef7e001b9c60
AryanChopra <aryanchopra989@gmail.com>
2025-02-19 17:38:46 +0530

    fixed button

--- src/app/components/NextButton.tsx
+++ src/app/components/NextButton.tsx
@@ -3,8 +3,8 @@ import { ArrowRight } from 'lucide-react';
 
 export default function NextButton() {
   return (
-    <div className="pt-2 pb-6 bg-black">
-      <button className="w-full bg-[#e5fb24] text-black hover:bg-opacity-90 transition-colors py-6 text-lg rounded-2xl font-semibold flex items-center justify-center gap-2">
+    <div className="pt-16 pb-8 bg-black">
+      <button className="w-full bg-[#E1F21A] text-black hover:bg-opacity-90 transition-colors py-6 text-lg rounded-2xl font-semibold flex items-center justify-center gap-2">
         NEXT <ArrowRight className="w-5 h-5" />
       </button>
     </div>
